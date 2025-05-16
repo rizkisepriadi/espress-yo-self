ccd8643fefc61b617ed277ef3ca588e54e3b24f1
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-16 20:02:34 +0700

    feat: implement AppException class and safeCall utility function for error handling

---
+++ lib/utils/safe_call.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:developer' as developer;
+
+import 'AppException.dart';
+
+
+Future<T> safeCall<T>(Future<T> Function() block, {String label = ''}) async {
+  try {
+    return await block();
+  } on FirebaseException catch (e) {
+    developer.log('Firestore error in $label: ${e.message}', error: e);
+    throw AppException(e.message ?? 'Terjadi kesalahan Firebase', code: e.code);
+  } catch (e, stack) {
+    developer.log('Unknown error in $label', error: e, stackTrace: stack);
+    rethrow;
+  }
+}
