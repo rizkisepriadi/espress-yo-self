f91277230d3fe26efc568e26334fb82ec3b61faa
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-25 10:07:56 +0700

    feat: add CouponButton widget for displaying coupon information with customizable title, icon, and description

---
+++ lib/presentation/common/coupon_button.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CouponButton extends StatefulWidget {
+  const CouponButton(
+      {super.key, this.title, this.onPressed, this.icon, this.description});
+
+  final String? title;
+  final VoidCallback? onPressed;
+  final Icon? icon;
+  final String? description;
+
+  @override
+  State<CouponButton> createState() => _CouponButtonState();
+}
+
+class _CouponButtonState extends State<CouponButton> {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(left: 8.w),
+      decoration: BoxDecoration(
+          border: Border.all(color: colorScheme.onSurface.withAlpha(150)),
+          borderRadius: BorderRadius.circular(20.r)),
+      child: Row(
+        children: [
+          Flexible(
+            flex: 4,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  widget.title!,
+                  style: textTheme.titleSmall
+                      ?.copyWith(fontWeight: FontWeight.w600),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 8.w),
+                Text(
+                  widget.description ?? '',
+                  style: textTheme.bodySmall,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 12.w),
+          Flexible(
+            flex: 2,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: widget.icon != null
+                  ? widget.icon!
+                  : SvgPicture.asset(
+                      "assets/svgs/cup_coupon_icon.svg",
+                      height: 112,
+                      width: 111,
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
