77b39f53a24231498ccfe150ff8bdf4317fe336b
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-25 17:02:36 +0700

    feat: add FloatingButton widget for QR code scanning with responsive design

---
+++ lib/presentation/common/floating_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FloatingButton extends StatelessWidget {
+  const FloatingButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      width: 102.w,
+      height: 102.h,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12, blurRadius: 8, offset: Offset(0, 4))
+          ]),
+      padding: EdgeInsets.all(12.w),
+      child: FloatingActionButton(
+        onPressed: () {},
+        elevation: 0,
+        backgroundColor: colorScheme.onSurface,
+        child: Icon(
+          Icons.qr_code_scanner,
+          size: 36.w,
+          color: Colors.white,
+        ),
+        shape: CircleBorder(),
+      ),
+    );
+  }
+}
