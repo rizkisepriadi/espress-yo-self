d12409497874bfad340b6962423ca0e2bb39ebf0
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-27 18:33:10 +0700

    feat: add providers for RewardViewmodel and StampProgressViewModel

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -14,9 +14,13 @@ import 'package:espress_yo_self/domain/usecases/get_reward_usecase.dart';
 import 'package:espress_yo_self/domain/usecases/stamp_progress_usecase.dart';
 import 'package:espress_yo_self/domain/usecases/transactions_usecase.dart';
 import 'package:espress_yo_self/presentation/viewmodels/auth_viewmodel.dart';
+import 'package:espress_yo_self/presentation/viewmodels/reward_viewmodel.dart';
 import 'package:espress_yo_self/presentation/viewmodels/user_viewmodel.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../domain/entities/stamp_progress_entity.dart';
+import '../presentation/viewmodels/stamp_progress_viewmodel.dart';
 
 // ==================== FIREBASE PROVIDERS ====================
 final firebaseFirestoreProvider = Provider<FirebaseFirestore>((ref) {
@@ -42,7 +46,8 @@ final rewardRepositoryProvider = Provider<RewardRepository>((ref) {
 });
 
 // Stamp Progress Repository
-final stampProgressRepositoryProvider = Provider<StampProgressRepository>((ref) {
+final stampProgressRepositoryProvider =
+    Provider<StampProgressRepository>((ref) {
   return StampProgressRepositoryImpl();
 });
 
@@ -58,17 +63,20 @@ final getUserUseCaseProvider = Provider<GetUserUsecase>((ref) {
   return GetUserUsecase(repository);
 });
 
-final updateUserPointsUseCaseProvider = Provider<UpdateUserPointsUsecase>((ref) {
+final updateUserPointsUseCaseProvider =
+    Provider<UpdateUserPointsUsecase>((ref) {
   final repository = ref.watch(userRepositoryProvider);
   return UpdateUserPointsUsecase(repository);
 });
 
-final updateStampProgressUseCaseProvider = Provider<UpdateStampProgressUsecase>((ref) {
+final updateStampProgressUseCaseProvider =
+    Provider<UpdateStampProgressUsecase>((ref) {
   final repository = ref.watch(userRepositoryProvider);
   return UpdateStampProgressUsecase(repository);
 });
 
-final updateUserProfileUseCaseProvider = Provider<UpdateUserProfileUsecase>((ref) {
+final updateUserProfileUseCaseProvider =
+    Provider<UpdateUserProfileUsecase>((ref) {
   final repository = ref.watch(userRepositoryProvider);
   return UpdateUserProfileUsecase(repository);
 });
@@ -80,7 +88,8 @@ final getAllRewardsUseCaseProvider = Provider<GetAllRewardsUsecase>((ref) {
   return GetAllRewardsUsecase(repository);
 });
 
-final getActiveRewardsUseCaseProvider = Provider<GetActiveRewardsUsecase>((ref) {
+final getActiveRewardsUseCaseProvider =
+    Provider<GetActiveRewardsUsecase>((ref) {
   final repository = ref.watch(rewardRepositoryProvider);
   return GetActiveRewardsUsecase(repository);
 });
@@ -95,29 +104,34 @@ final redeemRewardUseCaseProvider = Provider<RedeemRewardUsecase>((ref) {
   return RedeemRewardUsecase(repository);
 });
 
-final getUserRedemptionsUseCaseProvider = Provider<GetUserRedemptionsUsecase>((ref) {
+final getUserRedemptionsUseCaseProvider =
+    Provider<GetUserRedemptionsUsecase>((ref) {
   final repository = ref.watch(rewardRepositoryProvider);
   return GetUserRedemptionsUsecase(repository);
 });
 
-final hasUserRedeemedRewardUseCaseProvider = Provider<HasUserRedeemedRewardUsecase>((ref) {
+final hasUserRedeemedRewardUseCaseProvider =
+    Provider<HasUserRedeemedRewardUsecase>((ref) {
   final repository = ref.watch(rewardRepositoryProvider);
   return HasUserRedeemedRewardUsecase(repository);
 });
 
-final markRedemptionAsUsedUseCaseProvider = Provider<MarkRedemptionAsUsedUsecase>((ref) {
+final markRedemptionAsUsedUseCaseProvider =
+    Provider<MarkRedemptionAsUsedUsecase>((ref) {
   final repository = ref.watch(rewardRepositoryProvider);
   return MarkRedemptionAsUsedUsecase(repository);
 });
 
 // ==================== STAMP PROGRESS USECASES ====================
 
-final getStampProgressUseCaseProvider = Provider<GetStampProgressUsecase>((ref) {
+final getStampProgressUseCaseProvider =
+    Provider<GetStampProgressUsecase>((ref) {
   final repository = ref.watch(stampProgressRepositoryProvider);
   return GetStampProgressUsecase(repository);
 });
 
-final updateStampProgressRepoUseCaseProvider = Provider<UpdateStampProgressRepoUsecase>((ref) {
+final updateStampProgressRepoUseCaseProvider =
+    Provider<UpdateStampProgressRepoUsecase>((ref) {
   final repository = ref.watch(stampProgressRepositoryProvider);
   return UpdateStampProgressRepoUsecase(repository);
 });
@@ -129,17 +143,20 @@ final addTransactionUseCaseProvider = Provider<AddTransactionUsecase>((ref) {
   return AddTransactionUsecase(repository);
 });
 
-final getUserTransactionsUseCaseProvider = Provider<GetUserTransactionsUsecase>((ref) {
+final getUserTransactionsUseCaseProvider =
+    Provider<GetUserTransactionsUsecase>((ref) {
   final repository = ref.watch(transactionsRepositoryProvider);
   return GetUserTransactionsUsecase(repository);
 });
 
 // ==================== VIEWMODELS ====================
 
-final getUserViewModelProvider = StateNotifierProvider<UserViewModel, AsyncValue<UserEntity>>((ref) {
+final getUserViewModelProvider =
+    StateNotifierProvider<UserViewModel, AsyncValue<UserEntity>>((ref) {
   final getUserUsecase = ref.watch(getUserUseCaseProvider);
   final updateUserPointsUsecase = ref.watch(updateUserPointsUseCaseProvider);
-  final updateStampProgressUsecase = ref.watch(updateStampProgressUseCaseProvider);
+  final updateStampProgressUsecase =
+      ref.watch(updateStampProgressUseCaseProvider);
   final updateUserProfileUsecase = ref.watch(updateUserProfileUseCaseProvider);
   return UserViewModel(
     getUserUsecase: getUserUsecase,
@@ -149,7 +166,44 @@ final getUserViewModelProvider = StateNotifierProvider<UserViewModel, AsyncValue
   );
 });
 
-final authViewModelProvider = StateNotifierProvider<AuthViewModel, AsyncValue<User?>>((ref) {
+final authViewModelProvider =
+    StateNotifierProvider<AuthViewModel, AsyncValue<User?>>((ref) {
   final authService = ref.watch(authServiceProvider);
   return AuthViewModel(authService);
+});
+
+final rewardViewModelProvider =
+    StateNotifierProvider<RewardViewmodel, AsyncValue<List<dynamic>>>((ref) {
+  final getAllRewardsUsecase = ref.watch(getAllRewardsUseCaseProvider);
+  final getActiveRewardsUsecase = ref.watch(getActiveRewardsUseCaseProvider);
+  final getRewardByIdUsecase = ref.watch(getRewardByIdUseCaseProvider);
+  final redeemRewardUsecase = ref.watch(redeemRewardUseCaseProvider);
+  final getUserRedemptionsUsecase =
+      ref.watch(getUserRedemptionsUseCaseProvider);
+  final hasUserRedeemedRewardUsecase =
+      ref.watch(hasUserRedeemedRewardUseCaseProvider);
+  final markRedemptionAsUsedUsecase =
+      ref.watch(markRedemptionAsUsedUseCaseProvider);
+
+  return RewardViewmodel(
+    getAllRewardsUsecase: getAllRewardsUsecase,
+    getActiveRewardsUsecase: getActiveRewardsUsecase,
+    getRewardByIdUsecase: getRewardByIdUsecase,
+    redeemRewardUsecase: redeemRewardUsecase,
+    getUserRedemptionsUsecase: getUserRedemptionsUsecase,
+    hasUserRedeemedRewardUsecase: hasUserRedeemedRewardUsecase,
+    markRedemptionAsUsedUsecase: markRedemptionAsUsedUsecase,
+  );
+});
+
+final stampProgressViewModelProvider = StateNotifierProvider<
+    StampProgressViewModel, AsyncValue<StampProgressEntity>>((ref) {
+  final getStampProgressUsecase = ref.watch(getStampProgressUseCaseProvider);
+  final updateStampProgressRepoUsecase =
+      ref.watch(updateStampProgressRepoUseCaseProvider);
+
+  return StampProgressViewModel(
+    getStampProgressUsecase: getStampProgressUsecase,
+    updateStampProgressRepoUsecase: updateStampProgressRepoUsecase,
+  );
 });
