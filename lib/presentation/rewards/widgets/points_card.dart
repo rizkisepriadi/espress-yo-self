c549bcd55c6f582d7c09c7d2ac4c9a6c883dbbfe
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-06-02 06:18:08 +0700

    refactor: enhance PointsCard to include filledCups parameter and update coffee stack rendering

--- lib/presentation/rewards/widgets/points_card.dart
+++ lib/presentation/rewards/widgets/points_card.dart
@@ -4,8 +4,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class PointsCard extends StatefulWidget {
   final int points;
+  final int filledCups;
 
-  const PointsCard({super.key, required this.points});
+  const PointsCard({super.key, required this.points, required this.filledCups});
 
   @override
   State<PointsCard> createState() => _PointsCardState();
@@ -17,101 +18,98 @@ class _PointsCardState extends State<PointsCard> {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
 
-    return GestureDetector(
-      onTap: () {},
-      child: SizedBox(
-          child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(10.w),
-            decoration: BoxDecoration(
-              color: colorScheme.secondary,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(16.r),
-                  topRight: Radius.circular(16.r)),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Points",
-                  style: textTheme.titleMedium?.copyWith(
-                    fontWeight: FontWeight.w600,
-                    color: colorScheme.onSecondary,
-                  ),
-                ),
-                Text(
-                  '${widget.points} pts',
-                  style: textTheme.titleMedium?.copyWith(
-                    fontWeight: FontWeight.w600,
-                    color: colorScheme.onSecondary,
-                  ),
-                ),
-              ],
-            ),
+    return SizedBox(
+        child: Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(10.w),
+          decoration: BoxDecoration(
+            color: colorScheme.secondary,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16.r),
+                topRight: Radius.circular(16.r)),
           ),
-          Container(
-            padding: EdgeInsets.all(16.w),
-            decoration: BoxDecoration(
-              color: colorScheme.primary.withAlpha(204),
-              borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(16.r),
-                  bottomLeft: Radius.circular(16.r)),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Coffees Stack",
-                  style: textTheme.titleMedium?.copyWith(
-                    fontWeight: FontWeight.w600,
-                    color: colorScheme.onPrimary,
-                  ),
-                ),
-                _buildCoffeesStack(context),
-              ],
-            ),
-          ),
-          SizedBox(height: 16.h),
-          Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SizedBox(
-                width: 180.w,
-                child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: colorScheme.primary,
-                      foregroundColor: colorScheme.onPrimary,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.r),
-                      ),
-                    ),
-                    child: Text("Details")),
+              Text(
+                "Points",
+                style: textTheme.titleMedium?.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: colorScheme.onSecondary,
+                ),
               ),
-              SizedBox(
-                width: 180.w,
-                child: OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                      foregroundColor: colorScheme.primary,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.r),
-                      ),
-                    ),
-                    child: Text("Redeem")),
+              Text(
+                '${widget.points} pts',
+                style: textTheme.titleMedium?.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: colorScheme.onSecondary,
+                ),
               ),
             ],
           ),
-        ],
-      )),
-    );
+        ),
+        Container(
+          padding: EdgeInsets.all(16.w),
+          decoration: BoxDecoration(
+            color: colorScheme.primary.withAlpha(204),
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(16.r),
+                bottomLeft: Radius.circular(16.r)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Coffees Stack",
+                style: textTheme.titleMedium?.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: colorScheme.onPrimary,
+                ),
+              ),
+              _buildCoffeesStack(context, widget.filledCups),
+            ],
+          ),
+        ),
+        SizedBox(height: 16.h),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 180.w,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: colorScheme.primary,
+                    foregroundColor: colorScheme.onPrimary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.r),
+                    ),
+                  ),
+                  child: Text("Details")),
+            ),
+            SizedBox(
+              width: 180.w,
+              child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: colorScheme.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.r),
+                    ),
+                  ),
+                  child: Text("Redeem")),
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
 
-Widget _buildCoffeesStack(BuildContext context) {
+Widget _buildCoffeesStack(BuildContext context, int cups) {
   const int totalCups = 5;
-  const int filledCups = 3;
+  final int filledCups = cups;
 
   return Padding(
     padding: EdgeInsets.symmetric(vertical: 2.h),
